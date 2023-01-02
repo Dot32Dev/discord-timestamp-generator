1c4bcdebbea87119da4922dae819b2d3adbe9b62
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-01-02 13:38:32 +0800

    Fix JS not running, no longer setting innerHTML of deleted element

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ Date.prototype.addHours = function(h) {
 var newYears = new Date(Date.UTC(2023, 0, 1, 0, 0, 0))
 // var newYears = new Date(Date.UTC(2022, 11, 27, 0, 0, 0))
 newYears.addHours(timezone/60)
-document.getElementById("special-new-years").innerHTML = `&lt;t:${newYears.getTime()/1000}:R>`
+// document.getElementById("special-new-years").innerHTML = `&lt;t:${newYears.getTime()/1000}:R>`
 
 
 
