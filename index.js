5ba83964ba3f102d10f04a6acf08a40489bb4993
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-01-18 12:49:00 +0800

    Add back stats

--- index.js
+++ index.js
@@ -236,16 +236,16 @@ document.querySelector(".plus-minute").onclick = function() {
  AdjustMinutes(1) 
 }
 
-// fetch("https://api.github.com/repos/Dot32IsCool/discord-timestamp-generator")
-// .then(response => response.json())
-// .then(data => {
-//   console.log(data.stargazers_count)
-//   document.querySelector(".stars").innerHTML = `${data.stargazers_count} Stars`
-// })
+fetch("https://api.github.com/repos/Dot32IsCool/discord-timestamp-generator")
+.then(response => response.json())
+.then(data => {
+  console.log(data.stargazers_count)
+  document.querySelector(".stars").innerHTML = `${data.stargazers_count} Stars`
+})
 
-// fetch("https://discord.com/api/guilds/922185010205822976/widget.json")
-// .then(response => response.json())
-// .then(data => {
-//   console.log(data.presence_count)
-//   document.querySelector(".members").innerHTML = `${data.presence_count} Online`
-// })
+fetch("https://discord.com/api/guilds/922185010205822976/widget.json")
+.then(response => response.json())
+.then(data => {
+  console.log(data.presence_count)
+  document.querySelector(".members").innerHTML = `${data.presence_count} Online`
+})
